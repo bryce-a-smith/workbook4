9aca90aec3a576b1fe083df763a6d44c2806e251
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-10-30 14:56:48 -0400

    initial commit

---
+++ demos/1-5-javascript-objects.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let employee1 = {
+  employeeId: "1",
+  name: "Ezra",
+  jobTitle: "Theater Teacher",
+  payRate: 38.46,
+};
+
+let employee2 = {
+  employeeId: "2",
+  name: "Elisha",
+  jobTitle: "Game Programmer",
+  payRate: 43.27,
+};
+
+
