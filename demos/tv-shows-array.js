418b6884512a8cfc2083f137e33ed1198b2dc282
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-10-31 13:32:12 -0400

    .length

---
+++ demos/tv-shows-array.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let shows = ["Breaking Bad", "Better Call Saul", "Friends", "Supernatural"];
+
+let length = shows.length;
+
+for(let i = 0; i < length; i++) {
+    console.log(shows[i]);
+}
