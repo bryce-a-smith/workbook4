9cb34c399551459ba2874bde7ca538028814b33b
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-10-31 13:37:26 -0400

    exercise 1 arrays

---
+++ exercises/ArrayScripts/my_family.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let myFamily = ["Bryce", "Allie", "Hulanna", "Emmy", "Beethoven", "Frankie", "Timmy", "Gloria"];
+
+let length = myFamily.length;
+
+for(let i = 0; i < length; i++) {
+    console.log(myFamily[i]);
+}
