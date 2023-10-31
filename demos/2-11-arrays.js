635aefb0ff3d8a7ce92edfb933674d8b03a64c2f
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-10-31 13:29:20 -0400

    arrays

---
+++ demos/2-11-arrays.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let students = ["Talib", "Khalil", "Junaid", "Kaitlyn"];
+
+let studentsLength = students.length;
+
+for(let i = 0; i < studentsLength; i++) {
+    console.log(students[i]);
+}
+
